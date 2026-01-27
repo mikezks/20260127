c8f1c8842a5dc4cd6fbd7f9a351b3326f67daec2
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-01-27 16:45:56 +0100

    refactor: http resource

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -2,12 +2,16 @@ import { Component, effect, inject, input, numberAttribute } from '@angular/core
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
 import { validatePassengerStatus } from '../../util-validation/passenger-validator/passenger-status.validator';
+import { httpResource } from '@angular/common/http';
+import { initialPassenger, Passenger } from '../../logic-passenger/model/passenger';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './passenger-edit.component.html'
 })
@@ -24,7 +28,10 @@ export class PassengerEditComponent {
   });
 
   readonly id = input(0, { transform: numberAttribute });
-  protected readonly passengerResource = this.passengerService.findByIdAsResource(this.id);
+  protected readonly passengerResource = httpResource<Passenger>(() => ({
+    url: 'https://demo.angulararchitects.io/api/passenger',
+    params: { id: this.id() }
+  }), { defaultValue: initialPassenger });
 
   constructor() {
     effect(() => {
