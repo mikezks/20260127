43cb3dc8e9f66aa09c0127e6d128f127cb42682f
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-01-28 13:47:13 +0100

    refactor: custom validator

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,13 +1,33 @@
 import { httpResource } from '@angular/common/http';
-import { Component, input, numberAttribute, signal } from '@angular/core';
-import { form, FormField, required, schema } from '@angular/forms/signals';
+import { Component, input, numberAttribute } from '@angular/core';
+import { form, FormField, required, schema, SchemaPath, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger/model/passenger';
+
+export function validateFirstname(
+  firstname: SchemaPath<string>,
+  validFirstnames: string[]
+): void {
+  validate(firstname, ({ value }) => {
+    if (!validFirstnames.includes(value())) {
+      return {
+        kind: 'fordbiddenFirstname',
+        message:
+          'This firstname is not allowed. Please use one of the following: ' + validFirstnames.join(', ')
+      }
+    }
+
+    return null;
+  });
+}
 
 export const passengerSchema = schema<Passenger>(passengerPath => {
   required(passengerPath.name, {
     message: 'Please enter a name - this field is mandatory.'
   });
+  validateFirstname(passengerPath.firstName, [
+    'Emma', 'Mia', 'Hanna'
+  ])
 });
 
 @Component({
@@ -23,24 +43,15 @@ export class PassengerEditComponent {
   readonly id = input(0, { transform: numberAttribute });
   
   // (1) Data Model: Writable Signal
-  private readonly passenger = signal(initialPassenger);
-
-  // (2) Field State: value, valid, dirty, touched
-  protected readonly editForm = form(this.passenger, passengerSchema);
-
-
   protected readonly passengerResource = httpResource<Passenger>(() => ({
     url: 'https://demo.angulararchitects.io/api/passenger',
     params: { id: this.id() }
   }), { defaultValue: initialPassenger });
 
+  // (2) Field State: value, valid, dirty, touched
+  protected readonly editForm = form(this.passengerResource.value, passengerSchema);
+
   protected save(): void {
-    console.log(
-      this.editForm().value(),
-      this.passengerResource.value()
-    );
-    this.passengerResource.set(
-      this.editForm().value()
-    );
+    console.log(this.passengerResource.value());
   }
 }
